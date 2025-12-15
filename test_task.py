cdf081142aa6979e747b0da8ed4bd7100027c380
Foli <160885239+folixd@users.noreply.github.com>
2025-12-15 16:57:22 +0100

    Delete test_task.py

--- test_task.py
+++
@@ -1,9 +0,0 @@
-from task import Task
-
-def test_mark_done():
-    """
-    Tests that a task is correctly marked as completed.
-    """
-    task = Task("Test task")
-    task.mark_done()
-    assert task.completed is True
